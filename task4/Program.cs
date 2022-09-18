3cee2b91ae83624439f4e9f69f5e43721490c8ef
Nick <mikabrmail@gmail.com>
2022-09-18 17:38:58 +0300

    task4 complete

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму цифр числа
+
+int SummDigits(int number)
+{
+    return number == 0 ? 0 : number % 10 + SummDigits(number / 10);
+}
+
+Console.WriteLine($"Сумма цифр заданного числа равна {SummDigits(7453)}");
