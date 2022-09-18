f5623b67d2036b6d1e957a9f316b6bcd40894431
Nick <mikabrmail@gmail.com>
2022-09-18 17:38:22 +0300

    task2 complete

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void Numbers (int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write($"{m} ");
+        Numbers(m + 1, n);
+    }
+}
+
+Numbers(7, 15);
