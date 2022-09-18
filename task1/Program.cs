48aef0edaf763ed68534208bba2d97ded6e30156
Nick <mikabrmail@gmail.com>
2022-09-18 17:38:02 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+void Numbers (int n)
+{
+    if (n >= 1)
+    {
+        Console.Write($"{n} ");
+        Numbers(n - 1);
+    }
+}
+
+Numbers(15);
