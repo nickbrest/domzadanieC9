f043c3c4cc79c2569ef966d67a8817be08fe5996
Nick <mikabrmail@gmail.com>
2022-09-18 17:39:48 +0300

    task7 complete

---
+++ task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+
+int PsevdoFib(int first, int second, int n)
+{
+    return n == 1 ? first : (n == 2) ? second
+    : PsevdoFib(first, second, n - 1) + PsevdoFib(first, second, n - 2);
+}
+
+void PrintFib(int first, int second, int n)
+{
+    for (int i = 1; i <= n; i++)
+        Console.Write($"{PsevdoFib(first, second, i)} ");
+}
+
+PrintFib(7, 10, 9);
+// (первый элемент, второй элемент, N)
