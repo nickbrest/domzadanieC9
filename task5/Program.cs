de4d85d48535c9b6629c730efd7939242a2b2714
Nick <mikabrmail@gmail.com>
2022-09-18 17:39:15 +0300

    task5 complete

---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу вычисления функции Аккермана A(n, m)
+
+int Akkerman(int n, int m)
+{
+    return n == 0 ? m + 1 : n > 0 && m == 0 ? Akkerman(n - 1, 1) : Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+Console.WriteLine(Akkerman(3, 4));
