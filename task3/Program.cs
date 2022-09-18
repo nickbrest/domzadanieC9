d684ca2c2a8931454be3b8341c43c6907f24705a
Nick <mikabrmail@gmail.com>
2022-09-18 17:38:40 +0300

    task3 complete

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SummElements(int m, int n)
+{
+    return m == n ? m : m + SummElements(m + 1, n);
+}
+
+int summ = SummElements(3, 11);
+Console.WriteLine($"Сумма элементов равна {summ}");
